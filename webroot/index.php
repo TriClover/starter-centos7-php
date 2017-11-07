53b3cf2d6805e273291a2d56f4f1b83408d645f8
Anthony Heathcoat <heathcoata@midwestloanservices.com>
2017-11-07 15:10:25 -0500

    Add necessary packages to support schema versioning in ki framework

--- webroot/index.php
+++ webroot/index.php
@@ -1,25 +1,11 @@
 <?php
-require_once('vendor/mls/ki/ki.php');
-header(ki\htmlHeaders());
-ki\init();
+require_once('vendor/autoload.php');
+use \mls\ki\Security\Authenticator;
+use \mls\ki\Widgets\LoginForm;
+mls\ki\Ki::init();
 
-$fields = array();
-$fields[] = new ki\widgets\DataTableField('id');
-$fields[] = new ki\widgets\DataTableField('name', NULL, 'nomini', true, true, true, array('pattern'=>'^[a-zA-Z]*$'));
-$fields[] = new ki\widgets\DataTableField('enabled', NULL, NULL, true, true, 1);
-$fields[] = new ki\widgets\DataTableField('age', NULL, NULL, true, false, true, NULL);
-$fields[] = new ki\widgets\DataTableField('altname');
-$users = new ki\widgets\DataTable('users', 'ki_users', $fields, true, true, false, 3, false, false, false, false, NULL, array('Reset Password'=>'testCallback'));
-$users->handleParams();
-
-echo ki\pageHeader();
-echo $users->getHTML();
-echo ki\pageFooter();
-
-function testCallback($pk)
-{
-	echo "Test callback\n";
-	var_dump($pk);
-}
+echo mls\ki\MarkupGenerator::pageHeader();
+echo 'Hello World';
+echo mls\ki\MarkupGenerator::pageFooter();
 
 ?>
