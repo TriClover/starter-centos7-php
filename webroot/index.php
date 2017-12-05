83a77530da7ca660a7fd93bc00b070265d419337
Anthony Heathcoat <heathcoata@midwestloanservices.com>
2017-12-05 10:50:31 -0500

    Tenative LetsEncrypt integration

--- webroot/index.php
+++ webroot/index.php
@@ -1,11 +1,19 @@
 <?php
 require_once('vendor/autoload.php');
+use \mls\ki\Ki;
+use \mls\ki\MarkupGenerator;
 use \mls\ki\Security\Authenticator;
+use \mls\ki\Setup\Setup;
 use \mls\ki\Widgets\LoginForm;
-mls\ki\Ki::init();
 
-echo mls\ki\MarkupGenerator::pageHeader();
-echo 'Hello World';
-echo mls\ki\MarkupGenerator::pageFooter();
+//Ki::init('site1');
+
+$setup = new Setup('site1');
+$setup->handleParams();
+
+echo MarkupGenerator::pageHeader();
+//echo 'Hello World';
+echo $setup->getHTML();
+echo MarkupGenerator::pageFooter();
 
 ?>
